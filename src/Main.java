74fb7007638bbf3ae3d7f8597a1e22032c18f1c9
simonn <socmscas@gmail.com>
2024-12-02 21:19:27 +0800

    B
    	modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private JFrame mainFrame;
+    Main(String[] args) {
+        // Invoked on the event dispatching thread.
+        // Do any initialization here.
+        mainFrame = new JFrame();
+
+    }
+
+    public void show() {
+        // Show the UI.
+        mainFrame.setVisible(true);
+    }
+
+    public static void main(final String[] args) {
+        // Schedule a job for the event-dispatching thread:
+        // creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Main(args).show();
+            }
+        });
     }
 }
