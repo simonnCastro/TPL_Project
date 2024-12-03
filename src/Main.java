6efb065d453ef8e352b83ca9aee96808937379dd
simonn <socmscas@gmail.com>
2024-12-03 17:55:45 +0800

    B

--- src/Main.java
+++ src/Main.java
@@ -17,22 +17,46 @@ public class Main {
 
         JPanel mainPanel = new JPanel(new BorderLayout());
         JPanel p2 = new JPanel();
-        p2.setLayout(new GridLayout(5,0,10,100));
+        p2.setLayout(new BoxLayout(p2, BoxLayout.PAGE_AXIS));
         JPanel p3 = new JPanel();
 
-        JButton b1 = new JButton();
-        JButton b2 = new JButton();
-        JButton b3 = new JButton();
-        JButton b4 = new JButton();
-        JButton b5 = new JButton();
+        JButton b1 = new JButton("Open File");
+        JButton b2 = new JButton("Lexical Analysis");
+        JButton b3 = new JButton("Syntax Analysis");
+        JButton b4 = new JButton("Semantic Analysis");
+        JButton b5 = new JButton("Clear");
+
+        b1.setMaximumSize(new Dimension(200,0));
+        b2.setMaximumSize(new Dimension(200,0));
+        b3.setMaximumSize(new Dimension(200,0));
+        b4.setMaximumSize(new Dimension(200,0));
+        b5.setMaximumSize(new Dimension(200,0));
+
+        b1.setPreferredSize(new Dimension(0,50));
+        b2.setPreferredSize(new Dimension(0,50));
+        b3.setPreferredSize(new Dimension(0,50));
+        b4.setPreferredSize(new Dimension(0,50));
+        b5.setPreferredSize(new Dimension(0,50));
+
+        b1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        b2.setAlignmentX(Component.CENTER_ALIGNMENT);
+        b3.setAlignmentX(Component.CENTER_ALIGNMENT);
+        b4.setAlignmentX(Component.CENTER_ALIGNMENT);
+        b5.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         p2.setBackground(Color.blue);
         p2.setPreferredSize(new Dimension(width/4, height));
+        p2.add(Box.createVerticalGlue());
         p2.add(b1);
+        p2.add(Box.createVerticalGlue());
         p2.add(b2);
+        p2.add(Box.createVerticalGlue());
         p2.add(b3);
+        p2.add(Box.createVerticalGlue());
         p2.add(b4);
+        p2.add(Box.createVerticalGlue());
         p2.add(b5);
+        p2.add(Box.createVerticalGlue());
 
         p3.setBackground(Color.red);
         p3.setPreferredSize(new Dimension(width, height));
