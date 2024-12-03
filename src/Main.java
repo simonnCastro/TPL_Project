728eca8a3e971ab0e09f455806f248ea9198d548
simonn <socmscas@gmail.com>
2024-12-03 16:11:32 +0800

    deleted:    src/Button.java 	modified:   src/Main.java 	deleted:    src/Panel.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,45 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     private JFrame mainFrame;
+    private int width = 1008;
+    private int height= 567;
     Main(String[] args) {
         // Invoked on the event dispatching thread.
         // Do any initialization here.
         mainFrame = new JFrame();
         mainFrame.setTitle("TPL-Project");
-        mainFrame.setSize(1008, 567);
+        mainFrame.setSize(width, height);
         mainFrame.setLocationRelativeTo(null);
+        mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        mainFrame.setResizable(false);
 
-        Button openFile = new Button();
-        mainFrame.add(openFile);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JPanel p2 = new JPanel();
+        p2.setLayout(new GridLayout(5,0,10,100));
+        JPanel p3 = new JPanel();
 
+        JButton b1 = new JButton();
+        JButton b2 = new JButton();
+        JButton b3 = new JButton();
+        JButton b4 = new JButton();
+        JButton b5 = new JButton();
+
+        p2.setBackground(Color.blue);
+        p2.setPreferredSize(new Dimension(width/4, height));
+        p2.add(b1);
+        p2.add(b2);
+        p2.add(b3);
+        p2.add(b4);
+        p2.add(b5);
+
+        p3.setBackground(Color.red);
+        p3.setPreferredSize(new Dimension(width, height));
+
+        mainPanel.add(p2, BorderLayout.WEST);
+        mainPanel.add(p3, BorderLayout.CENTER);
+        mainFrame.add(mainPanel);
 
     }
 
